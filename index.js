a2d651bccacf1c4f65012b1d3d3b570dd043c2e9
fadime94 <fadime.ozdemir11@gmail.com>
2020-07-20 14:55:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let headingToUpdate = document.querySelector( 'p' );
+    return headingToUpdate.innerHTML="This is really cool!";
+  });
