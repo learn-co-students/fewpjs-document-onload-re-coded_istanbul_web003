a429242cfa99bf2d9a33007abed015a4cab1b3be
Hiba <hiba.mashfij@gmail.com>
2020-07-26 13:43:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+const element = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    element.innerHTML = "This is really cool!";
+  });
+   
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+
+
+
+//   Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+
+// Note: Using the innerText property to modify DOM element content will not work for this lab. Use textContent or innerHTML instead.
+
+// Test your event in the browser to confirm that it is working.
