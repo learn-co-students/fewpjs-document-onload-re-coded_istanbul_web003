3145f6fd0922d1f9aa46dabf451d52b2ad4843f9
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-08-02 17:22:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//   dode()
+// });
+// function dode() {
+//   document.getElementById("text").innerHTML = "This is really cool!"
+// }
+//another way to solve it
+
+document.addEventListener("DOMContentLoaded", function dode() {
+  document.getElementById("text").innerHTML = "This is really cool!"
+});
